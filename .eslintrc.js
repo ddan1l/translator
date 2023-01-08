34df1cab4ce26abf677cdb13500c85a31815af6e
ddan1l <pospgv14@gmail.com>
2023-01-09 01:34:23 +0300

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "new-cap": "off",
     "no-console": "off",
     "no-shadow": "off",
     "prettier/prettier": "error",
@@ -34,5 +35,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off'
   },
 };
